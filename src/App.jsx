3aa0e749e8ccc3f6973b380bff8640248ee933e4
Khushi Patel <khushipatel3340@gmail.com>
2023-07-31 00:54:43 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './Header'
+
+function App() {
+  return (
+    <Router>
+      <div className='app'>
+        <Routes>
+          <Route path='/checkout' element={<h1>Checkout</h1>}/>
+          <Route path='/login' element={<h1>Login</h1>}/>
+          <Route path='/' element={<Header />} />
+        </Routes>
+      </div>
+    </Router>
+  )
+}
+
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import "./Header.css";
+import { Link } from 'react-router-dom';
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket"; 
+
+function Header() {
+  return (
+    <nav className='header'>
+      <Link to="/">
+        <img 
+          className='header--logo' 
+          src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="amazon-logo" 
+        />
+      </Link>
+
+      <div className="header--search">
+        <input type="text" className="header--searchInput" />
+        <SearchIcon className="header--searchIcon" />
+      </div>
+
+      <div className="header--nav">
+        <Link to="/login" className='header--link'>
+          <div className='header--option'>
+            <span className="header--optionLineOne">Hello Khushi</span>
+            <span className="header--optionLineTwo">Sign In</span>
+          </div>
+        </Link>
+
+        <Link to="/" className='header--link'>
+          <div className='header--option'>
+            <span className="header--optionLineOne">Returns</span>
+            <span className="header--optionLineTwo">& Orders</span>
+          </div>
+        </Link>
+
+        <Link to="/" className='header--link'>
+          <div className='header--option'>
+            <span className="header--optionLineOne">Your</span>
+            <span className="header--optionLineTwo">Prime</span>
+          </div>
+        </Link>
+
+        <Link to="/checkout" className='header--link'>
+          <div className="header--optionBasket">
+            <ShoppingBasketIcon />
+            <span className='header--optionLineTwo header--basketCount'>0</span>
+          </div>
+        </Link>
+      </div>
+
+    </nav>
+  )
+}
+
+export default Header
